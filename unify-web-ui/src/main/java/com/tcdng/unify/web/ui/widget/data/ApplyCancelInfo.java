1c2da08072e743b4c2518f965816f9920c489a2c
Lateef Ojulari <lateef.ojulari@tcdng.com>
2025-07-15 13:24:09 +0100

    Implement apply cancel dialog

---
+++ unify-web-ui/src/main/java/com/tcdng/unify/web/ui/widget/data/ApplyCancelInfo.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2018-2025 The Code Department.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.tcdng.unify.web.ui.widget.data;
+
+/**
+ * Apply-cancel information.
+ * 
+ * @author The Code Department
+ * @since 4.1
+ */
+public class ApplyCancelInfo {
+
+	private String dialogCaption;
+
+	private String applyCaption;
+
+	public ApplyCancelInfo(String dialogCaption, String applyCaption) {
+		this.dialogCaption = dialogCaption;
+		this.applyCaption = applyCaption;
+	}
+
+	public String getDialogCaption() {
+		return dialogCaption;
+	}
+
+	public String getApplyCaption() {
+		return applyCaption;
+	}
+		
+}
