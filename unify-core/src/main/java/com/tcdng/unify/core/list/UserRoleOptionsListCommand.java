33f75e89b1e1598b638fb6378d7c7606a48e9221
Lateef Ojulari <lateef.ojulari@tcdng.com>
2026-08-10 01:01:08 +0100

    Implement user role options list command

---
+++ unify-core/src/main/java/com/tcdng/unify/core/list/UserRoleOptionsListCommand.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2018-2026 The Code Department.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.tcdng.unify.core.list;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import com.tcdng.unify.common.data.Listable;
+import com.tcdng.unify.core.UnifyException;
+import com.tcdng.unify.core.UserToken;
+import com.tcdng.unify.core.annotation.Component;
+
+/**
+ * User role options list command.
+ * 
+ * @author The Code Department
+ * @since 4.1
+ */
+@Component("userroleoptionslist")
+public class UserRoleOptionsListCommand extends AbstractZeroParamsListCommand {
+
+	@Override
+	public List<? extends Listable> execute(Locale locale, ZeroParams params) throws UnifyException {
+		final UserToken userToken = getUserToken();
+		List<? extends Listable> list = userToken != null ? userToken.getRoleOptions() : null;
+		return list != null ? list : Collections.emptyList();
+	}
+}
